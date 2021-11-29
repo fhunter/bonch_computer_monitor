fa515e4c857642c0e12bb315ac322db201d99f31
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2021-11-29 18:10:39 +0300

    Added two tables for sessions (user and computer)

--- database.sql
+++ database.sql
@@ -6,6 +6,8 @@ CREATE TABLE uptime (id integer primary key autoincrement not null, ip text not 
 CREATE TABLE users (id integer primary key autoincrement not null, ip text not null,  time datetime not null, users text);
 CREATE TABLE load (id integer primary key autoincrement not null, ip text not null, time datetime not null, cpuload real not null, loadavg real not null, cores integer not null);
 CREATE TABLE cmos (id integer primary key autoincrement not null, hostname text unique not null, time datetime not null, cmos real not null);
+CREATE TABLE user_sessions (id integer primary key autoincrement not null, hostname text not null, starttime datetime not null, endtime datetime, users text);
+CREATE TABLE host_sessions (id integer primary key autoincrement not null, hostname text not null, starttime datetime not null, endtime datetime);
 CREATE UNIQUE INDEX hostnames_index on hostnames(ip, time);
 CREATE INDEX load_index1 on load(ip, time);
 CREATE INDEX users_index1 on users(ip, time);
