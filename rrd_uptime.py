6561cf22e8b24006603df93832ef41cd4f29f97b
Nikolay Zamotaev <fhunter@fhunter.mine.nu>
2021-12-05 01:42:23 +0300

    Implemented uptime rrd

--- rrd_uptime.py
+++ rrd_uptime.py
@@ -1,4 +1,6 @@
+import os
 import rrdtool
+
 
 def graph(hostname):
     test = rrdtool.graphv("-", "--start", "-1y", "-w 800", "--title=Uptime %s" % hostname,
@@ -9,8 +11,54 @@ def graph(hostname):
         )
     return test['image']
 
-def insert(hostname, data): #FIXME - implement
-    return ""
+def insert(hostname, data, timestampt="N"):
+    if not exists(hostname):
+        create(hostname)
+    rrdname = "rrds/" + hostname + "_uptime.rrd"
+    rrdtool.update(rrdname, '%s:%s' % (timestamp, data[0]))
 
-def last(hostname): #FIXME - implement
-    return -1
+def exists(hostname):
+    rrdname = "rrds/" + hostname + "_uptime.rrd"
+    return os.path.exists(rrdname)
+
+
+def create(hostname):
+    if not exists(hostname):
+        rrdname = "rrds/" + hostname + "_ansible.rrd"
+        rrdtool.create(rrdname, '--start', '-2years',
+                       '--step', '900',
+                       'DS:uptime:GAUGE:1200:0:315360000',
+                       'RRA:AVERAGE:0.5:1:1200',
+                       'RRA:AVERAGE:0.5:6:1200',
+                       'RRA:AVERAGE:0.5:24:1200',
+                       'RRA:MIN:0.5:1:1200',
+                       'RRA:MIN:0.5:6:1200',
+                       'RRA:MIN:0.5:24:1200',
+                       'RRA:MAX:0.5:1:1200',
+                       'RRA:MAX:0.5:6:1200',
+                       'RRA:MAX:0.5:24:1200',
+                       'RRA:LAST:0.5:1:1200',
+                       'RRA:LAST:0.5:6:1200',
+                       'RRA:LAST:0.5:24:1200'
+                      )
+        return True
+    else:
+        return False
+
+
+def last(hostname):
+    rrdname = "rrds/" + hostname + "_uptime.rrd"
+    try:
+        last = rrdtool.last(rrdname)
+    except:
+        return None
+    return last
+
+def latest(hostname):
+    rrdname = "rrds/" + hostname + "_uptime.rrd"
+    try:
+        info = rrdtool.info(rrdname)
+        lastupdate = [info['last_update'], info['ds[uptime].last_ds']]
+        return lastupdate
+    except:
+        return None
