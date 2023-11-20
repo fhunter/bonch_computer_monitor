7cb334d832c2068b3acb53e51ce1302cdd517216
fhunter <fhunter@fhunter.link>
2023-11-20 17:53:02 +0100

    Added room check

--- computer.py
+++ computer.py
@@ -1,5 +1,5 @@
 import datetime
-from my_db import Computer
+from my_db import Computer, Room
 
 def add_or_update(session, machineid, ip_addr, reportedhostname):
     computer = session.query(Computer).filter(Computer.machineid == machineid).filter(Computer.ip == ip_addr).filter(Computer.hostname == reportedhostname).first()
@@ -7,6 +7,10 @@ def add_or_update(session, machineid, ip_addr, reportedhostname):
         computer.last_report = datetime.datetime.now()
         session.commit()
     else:
-        computer = Computer(hostname = reportedhostname, ip = ip_addr, machineid = machineid)
+        room = session.query(Room).filter(Room.name == "Прочее").first()
+        if not room:
+            room = Room(name = "Прочее")
+            session.add(Room)
+        computer = Computer(hostname = reportedhostname, ip = ip_addr, machineid = machineid, room = room.id)
         session.add(computer)
         session.commit()
