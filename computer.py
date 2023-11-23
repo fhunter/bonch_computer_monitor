54447c51f49f1c834773f5da41086ddf25ed0c89
fhunter <fhunter@fhunter.link>
2023-11-23 16:57:50 +0100

    Pylint fixes

--- computer.py
+++ computer.py
@@ -1,16 +1,26 @@
+""" Computer registration module"""
+
 import datetime
 from my_db import Computer, Room
 
-def add_or_update(session, machineid, ip_addr, reportedhostname):
-    computer = session.query(Computer).filter(Computer.machineid == machineid).filter(Computer.ip == ip_addr).filter(Computer.hostname == reportedhostname).first()
+def add_or_update(db_session, machineid, ip_addr, reportedhostname):
+    """ Register computer in database by machineid, ip and hostname """
+    computer = (db_session.query(Computer)
+               .filter(Computer.machineid == machineid)
+               .filter(Computer.ip == ip_addr)
+               .filter(Computer.hostname == reportedhostname)
+               .first())
     if computer:
         computer.last_report = datetime.datetime.now()
-        session.commit()
+        db_session.commit()
     else:
-        room = session.query(Room).filter(Room.name == "Прочее").first()
+        room = db_session.query(Room).filter(Room.name == "Прочее").first()
         if not room:
             room = Room(name = "Прочее")
-            session.add(Room)
-        computer = Computer(hostname = reportedhostname, ip = ip_addr, machineid = machineid, room = room.id)
-        session.add(computer)
-        session.commit()
+            db_session.add(Room)
+        computer = Computer(hostname = reportedhostname,
+                            ip = ip_addr,
+                            machineid = machineid,
+                            room = room.id)
+        db_session.add(computer)
+        db_session.commit()
